a38d61cd9518bfbff597967bf3c7e96545196e10
kloron <bszabogyula@gmail.com>
2022-05-08 14:52:06 +0200

    Initial commit

---
+++ app/src/main/java/com/example/cinema/ShopingItemAdapter.java
@@ -0,0 +1,134 @@
+package com.example.cinema;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+public class ShopingItemAdapter extends RecyclerView.Adapter<ShopingItemAdapter.ViewHolder> implements Filterable {
+    private ArrayList<ShopingItem> mShoppingData;
+    private ArrayList<ShopingItem> mSoppingDataAll;
+    private Context mContext;
+    private int lastPosition = -1;
+
+    ShopingItemAdapter(Context context, ArrayList<ShopingItem> itemsData) {
+        this.mShoppingData = itemsData;
+        this.mSoppingDataAll = itemsData;
+        this.mContext = context;
+    }
+
+    @Override
+    public ShopingItemAdapter.ViewHolder onCreateViewHolder(
+            ViewGroup parent, int viewType) {
+        return new ViewHolder(LayoutInflater.from(mContext)
+                .inflate(R.layout.list_item, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(ShopingItemAdapter.ViewHolder holder, int position) {
+        // Get current sport.
+        ShopingItem currentItem = mShoppingData.get(position);
+
+        // Populate the textviews with data.
+        holder.bindTo(currentItem);
+
+
+        if(holder.getAdapterPosition() > lastPosition) {
+            Animation animation = AnimationUtils.loadAnimation(mContext, R.anim.slide_in_row);
+            holder.itemView.startAnimation(animation);
+            lastPosition = holder.getAdapterPosition();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return mShoppingData.size();
+    }
+
+
+    /**
+     * RecycleView filter
+     * **/
+    @Override
+    public Filter getFilter() {
+        return shoppingFilter;
+    }
+
+    private Filter shoppingFilter = new Filter() {
+        @Override
+        protected FilterResults performFiltering(CharSequence charSequence) {
+            ArrayList<ShopingItem> filteredList = new ArrayList<>();
+            FilterResults results = new FilterResults();
+
+            if(charSequence == null || charSequence.length() == 0) {
+                results.count = mSoppingDataAll.size();
+                results.values = mSoppingDataAll;
+            } else {
+                String filterPattern = charSequence.toString().toLowerCase().trim();
+                for(ShopingItem item : mSoppingDataAll) {
+                    if(item.getName().toLowerCase().contains(filterPattern)){
+                        filteredList.add(item);
+                    }
+                }
+
+                results.count = filteredList.size();
+                results.values = filteredList;
+            }
+
+            return results;
+        }
+
+        @Override
+        protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+            mShoppingData = (ArrayList)filterResults.values;
+            notifyDataSetChanged();
+        }
+    };
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+        // Member Variables for the TextViews
+        private TextView mTitleText;
+        private TextView mInfoText;
+        private TextView mPriceText;
+        private ImageView mItemImage;
+        private RatingBar mRatingBar;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+
+            // Initialize the views.
+            mTitleText = itemView.findViewById(R.id.itemTitle);
+            mInfoText = itemView.findViewById(R.id.subTitle);
+            mItemImage = itemView.findViewById(R.id.itemImage);
+            mRatingBar = itemView.findViewById(R.id.ratingBar);
+            mPriceText = itemView.findViewById(R.id.price);
+        }
+
+        void bindTo(ShopingItem currentItem){
+            mTitleText.setText(currentItem.getName());
+            mInfoText.setText(currentItem.getInfo());
+            mPriceText.setText(currentItem.getPrice());
+            mRatingBar.setRating(currentItem.getRatedInfo());
+
+            // Load the images into the ImageView using the Glide library.
+            Glide.with(mContext).load(currentItem.getImageResource()).into(mItemImage);
+
+            itemView.findViewById(R.id.add_to_cart).setOnClickListener(view -> ((ShopListActivity)mContext).updateAlertIcon(currentItem));
+            itemView.findViewById(R.id.delete).setOnClickListener(view -> ((ShopListActivity)mContext).deleteItem(currentItem));
+        }
+    }
+
+}
